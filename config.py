61759c6b8320a2fe2688a782eda22d0037173a0c
antoo69 <antobarry69@gmail.com>
2024-09-27 00:23:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,22 +5,22 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7619388917:AAFVQCD9u_2fKYFznCvdUZA21OyY_9bkbtU")
 BOT_WORKERS = int(os.environ.get("BOT_WORKERS", "4"))
 
-APP_ID = int(os.environ.get("APP_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+APP_ID = int(os.environ.get("APP_ID", "22355402"))
+API_HASH = os.environ.get("API_HASH", "5d7858e035599aa080d65e14e5e34d4d")
 
-LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", ""))
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002057319198"))
 
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "izzyganteng")
-BROADCAST_AS_COPY = True
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://ferdisyrl:buburayam1@cluster0.89myp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "ferdisyrl")
+BROADCAST_AS_COPY = False
 
 PLUG = dict(root="antigcast/plugins")
 
-OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "").split())]
-OWNER_NAME = os.environ.get("OWNER_NAME", "")
+OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "7083782157 1506963557").split())]
+OWNER_NAME = os.environ.get("OWNER_NAME", "@fsyrl @fsyrl9")
 
 
 LOG_FILE_NAME = "antigcast_logs.txt"
